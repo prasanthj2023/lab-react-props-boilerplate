40df41e79ae9a60394068b295d0a7c2bff9fe40c
prasanthj2023 <prasanth.j@kalvium.community>
2024-01-22 10:14:04 +0530

    Updated

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,27 +1,5 @@
 import "./App.css";
-import elephant from "./images/elephant.jpeg";
-
-const imageData = () => {
-  let data = [
-    {
-      id: 1,
-      img: elephant,
-    },
-    {
-      id: 2,
-      img: elephant,
-    },
-    {
-      id: 3,
-      img: elephant,
-    },
-    {
-      id: 4,
-      img: elephant,
-    },
-  ];
-  return data;
-};
+import imageData from "./DataComponent";
 
 function App() {
   const image = imageData();
